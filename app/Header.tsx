acfa99b83534a244b5fb16bb1e83f93dcbbaff82
Lucas Menke <lucas_menke@icloud.com>
2023-01-22 13:55:47 +0100

    fixed hydration error

--- app/Header.tsx
+++ app/Header.tsx
@@ -5,7 +5,7 @@ export default function Header() {
     <header>
       <div className="grid grid-cols-3 items-center p-10">
         <Bars3Icon className="h-8 w-8 cursor-pointer" />
-        <Link href="/" prefetch={false}>
+        <Link href="/">
           <h1 className="text-4xl text-center font-serif">
             <span className="underline decoration-purple-700 decoration-4">
               MEETUP
@@ -13,7 +13,7 @@ export default function Header() {
             Club
           </h1>
         </Link>
-        <Link href="/meetups" prefetch={false} className="flex justify-end">
+        <Link href="/meetups" className="flex justify-end">
           <button
             className="hidden md:inline bg-slate-900 text-white px-4 lg:px-8 py-2 lg:py-4 rounded-full font-semibold
           border-2 border-slate-900 hover:bg-white hover:text-slate-900 hover:border-2 hover:border-slate-900"
