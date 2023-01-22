7e140c909fd8286b8b81ef558d632dd853fefed8
Lucas Menke <lucas_menke@icloud.com>
2023-01-22 14:04:55 +0100

    improved styling

--- app/MeetupCard.tsx
+++ app/MeetupCard.tsx
@@ -19,8 +19,8 @@ export default function MeetupCard({ meetup }: Props) {
   };
 
   return (
-    <div className="w-96 md:w-[600px] flex flex-row roundend-lg bg-slate-100 shadow-lg hover:scale-105 hover:shadow-xl hover:bg-slate-200 transition-all duration-200 ease-out">
-      <div className="w-40 md:w-60 h-44 md:h-56 object-cover rounded-l-md shadow-sm overflow-hidden">
+    <div className="w-96 md:w-[600px] flex flex-row rounded-xl bg-slate-100 shadow-lg hover:scale-105 hover:shadow-xl hover:bg-slate-200 transition-all duration-200 ease-out">
+      <div className="w-40 md:w-60 h-44 md:h-56 object-cover rounded-l-xl shadow-sm overflow-hidden">
         {meetup.image === null ? (
           <UserGroupIcon className="h-8 w-8 cursor-pointer text-white bg-purple-700" />
         ) : (
